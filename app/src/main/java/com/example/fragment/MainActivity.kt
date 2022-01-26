283659b9d2b8f01cd628ee6bf6fbc06f01632f3a
Jamshid <jamshid916781264@gmail.com>
2022-01-26 15:24:31 +0500

    instagram insta

--- app/src/main/java/com/example/fragment/MainActivity.kt
+++ app/src/main/java/com/example/fragment/MainActivity.kt
@@ -2,44 +2,30 @@ package com.example.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_blank.*
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val secondFragment = SecondFragment()
-        val homeFragment  = HomeFragment()
-        val  fragmentManager = supportFragmentManager
-        btn_add.setOnClickListener {
-           fragmentManager.beginTransaction()
-               .add(R.id.liner_contenier, homeFragment)
-               .addToBackStack(homeFragment.toString())
-               .commit()
-        }
-        btn_Remove.setOnClickListener {
-            fragmentManager.beginTransaction()
-                .remove(homeFragment)
-                .commit()
-        }
-        btn_hide.setOnClickListener {
-            fragmentManager.beginTransaction()
-                .hide(homeFragment)
-                .commit()
-        }
-        btn_Show.setOnClickListener {
-            fragmentManager.beginTransaction()
-                .show(homeFragment)
-                .commit()
-        }
-        btn_replace.setOnClickListener {
-            fragmentManager.beginTransaction()
-                .addToBackStack(secondFragment.toString())
-                .replace(R.id.liner_contenier, secondFragment)
-                .commit()
+        val homeFragment = HomeFragment()
+        val fragmentManager = supportFragmentManager
 
-        }
+        fragmentManager.beginTransaction().add(R.id.my_container,homeFragment).commit()
 
-
+        home.setOnClickListener {
+        fragmentManager.beginTransaction().replace(R.id.my_container ,secondFragment).commit()
     }
-}
+        My_profile.setOnClickListener {
+            fragmentManager.beginTransaction().replace(R.id.my_container , homeFragment).commit()
+        }
+
+
+
+
+        }
+    }
